abe8b7530ddb55e25c880f075d247e1b223162de
rayen troudi <26983014rayen@gmail.com>
2023-11-19 12:29:57 +0100

    final version

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/expenses', {
+mongoose.connect('mongodb://mongodb:27017/expenses', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 
 const expenseSchema = new mongoose.Schema({
   title: String,
